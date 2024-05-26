11c51d474748ec91a42b3a62cbd451931fff586d
mhasan0505 <mhasan.coder@gmail.com>
2024-05-27 03:39:32 +0600

    vercel

--- src/module/order/order.controller.ts
+++ src/module/order/order.controller.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
+import { OrderService } from "./order.service";
 // import orderService from "./order.service";
 // Create a new order
 
 const createOrder = async (req: Request, res: Response) => {
   const { email, productId, price, quantity } = req.body;
-  const result = await createOrder({ email, productId, price, quantity });
+  const result = await OrderService.createOrder({ email, productId, price, quantity });
   res.json({
     success: true,
     message: "Product created successfully!",
